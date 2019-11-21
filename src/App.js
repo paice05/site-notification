974d0853e05a25533b8ae0b2c65c5f8324a2e36d
Matheus Paice <matheuspaice@mandeumzap.com.br>
2019-11-21 10:28:16 -0300

    projeto com notificacoes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import toastr from "toastr";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => toastr.info("16/09 não havera expediente", "Aviso!", {
+          progressBar: true,
+          closeButton: true,
+          timeOut: 3000,
+          onHidden: () => console.log('finalizou')
+        })}
+      > notification info </button>
     </div>
   );
 }
